b3658894a7759a721c77a1ec2f9e543c42bcfd12
Stellar4 <andrey951768@gmail.com>
2022-01-03 19:44:38 +1000

    task 4

---
+++ Functions.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Npgsql;
+using System.Text.RegularExpressions;
+
+
+namespace DataBaseApp {
+    class Functions {
+
+        public static void AddGuild(string name, int fund, int chief) {
+            name = new Regex(@"\W").Replace(name, "");
+            using (NpgsqlConnection con = GetConnection()) {
+                if (!checkPlayerPresence(chief)) {
+                    Console.WriteLine("No such player. To get the list of available players, print 'get_players'");
+                }
+                string query = @$"INSERT INTO Guilds (guild_id, guild_name, fund, chief, foundation_time)
+                                    VALUES ({getLastGuildId() + 1}, '{name}', {fund}, {chief}, DATE(NOW()));";
+                con.Open();
+                var command = new NpgsqlCommand(query, con);
+                try {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public static void AlterRelation(int guild_1, int guild_2, string relation) {
+            var event_id = relation == "war" ? 9 : relation == "peace" ? 10 : relation == "alliance" ? 11 : -1;
+            if (event_id == -1) {
+                Console.WriteLine("Invalid relation");
+                return;
+            }
+            using (NpgsqlConnection con = GetConnection()) {
+                con.Open();
+                var command = new NpgsqlCommand(@$"INSERT INTO Logs (e_time, player_id, player_2_id, guild_id, guild_2_id, event_id)
+                                                VALUES (NOW(), NULL, NULL, {guild_1}, {guild_2}, {event_id})", con);
+                try {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                }
+            }
+
+        }
+
+        public static string GetRelation(int guild_1, int guild_2) {
+            using (NpgsqlConnection con = GetConnection()) {
+                con.Open();
+                var command = new NpgsqlCommand(@$"SELECT event_id FROM Logs WHERE guild_id = {guild_1} AND guild_2_id = {guild_2}
+                                                    OR guild_2_id = {guild_1} AND guild_id = {guild_2}
+                                                    ORDER BY e_time DESC LIMIT 1", con);
+                try {
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    if (!reader.HasRows || reader.GetInt32(0) == 10) {
+                        return "peace";
+                    }
+                    else if (reader.GetInt32(0) == 9) {
+                        return "war";
+                    }
+                    return "alliance";
+                }
+                catch (Exception e) {
+                    return Convert.ToString(e);
+                }
+                
+                
+            }
+        }
+
+        public static void getPlayers() {
+            NpgsqlConnection con = GetConnection();
+            con.Open();
+            string query = @"SELECT * FROM Players";
+            var command = new NpgsqlCommand(query, con);
+            try {
+                NpgsqlDataReader reader = command.ExecuteReader();
+
+                using (reader) {
+                    Console.WriteLine("id, name, level, money, guild_id:");
+                    while (reader.Read()) {
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            for (int j = 0; j < 5; j++) {
+                                Console.Write(reader[i + j]);
+                                Console.Write(", ");
+                            }
+                            i += 5;
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void getGuilds() {
+            NpgsqlConnection con = GetConnection();
+            con.Open();
+            string query = @"SELECT * FROM Guilds";
+            var command = new NpgsqlCommand(query, con);
+            try {
+                NpgsqlDataReader reader = command.ExecuteReader();
+
+                using (reader) {
+                    Console.WriteLine("id, name, fund, chief, foundation_time, dismission_time:");
+                    while (reader.Read()) {
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            for (int j = 0; j < 6; j++) {
+                                Console.Write(reader[i + j]);
+                                Console.Write(", ");
+                            }
+                            i += 6;
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static int getLastGuildId() {
+            using (NpgsqlConnection con = GetConnection()) {
+                con.Open();
+                var command = new NpgsqlCommand(@"SELECT guild_id FROM Guilds ORDER BY guild_id DESC LIMIT 1", con);
+                try {
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    return reader.GetInt32(0);
+                } catch (Exception e){
+                    Console.WriteLine(e);
+                    return -1;
+                }
+            }
+        }
+
+        public static int getLastPlayerId() {
+            using (NpgsqlConnection con = GetConnection()) {
+                con.Open();
+                var command = new NpgsqlCommand(@"SELECT player_id FROM Players ORDER BY player_id DESC LIMIT 1", con);
+                try {
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    return reader.GetInt32(0);
+                } catch (Exception e){
+                    Console.WriteLine(e);
+                    return -1;
+                }
+            }
+        }
+
+        public static bool checkPlayerPresence(int player_id) {
+            using (NpgsqlConnection con = GetConnection()) {
+                con.Open();
+                var command = new NpgsqlCommand(@$"SELECT COUNT(*) FROM Players WHERE player_id = {player_id}", con);
+                NpgsqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if (reader.GetInt32(0) > 0) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        private static NpgsqlConnection GetConnection() {
+            return new NpgsqlConnection(@"Server=localhost; Port=5432; User Id = postgres; Password=root; DataBase=postgres");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+
+namespace DataBaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Build_db.Build();
+            string input;
+            while (true) {
+                input = Console.ReadLine();
+                string[] command = input.Split(' ');
+                if (input == "exit" || input == "quit" || input == "q") {
+                    break;
+                }
+
+                if (input == "help") {
+                    Console.WriteLine("List of available commands:");
+                    Console.WriteLine("add_guild [name] [fund] [chief]");
+                    Console.WriteLine("get_players");
+                    Console.WriteLine("get_guilds");
+                    Console.WriteLine("get_relation [guild_1] [guild_2]");
+                    Console.WriteLine("alter_relation [guild_1] [guild_2] [new_relation], where [new_relation] can be: war, peace, alliance");
+                }
+                else if (command[0] == "add_guild") {
+                    bool i1 = true, i2 = true, r1 = true, r2 = true;
+                    int guild_fund = -1, guild_chief = -1;
+
+                    try {
+                        guild_fund = Convert.ToInt32(command[2]);
+                    } catch (FormatException) {
+                        i1 = false;
+                    } catch (IndexOutOfRangeException) {
+                        r1 = false;
+                    }
+                    try {
+                        guild_chief = Convert.ToInt32(command[3]);
+                    } catch (FormatException) {
+                        i2 = false;
+                    } catch (IndexOutOfRangeException) {
+                        r2 = false;
+                    }
+                    if (!i1) {
+                        Console.WriteLine("Fund must be integer");}
+                    if (!i2) {
+                        Console.WriteLine("Chief id must be integer");}
+                    if (!r1) {
+                        Console.WriteLine("No fund information provided");}
+                    if (!r2) {
+                        Console.WriteLine("No chief information provided");}
+                    if (command.Length < 2) {
+                        Console.WriteLine("No name provided");
+                    } else if (guild_chief == -1 || guild_fund == -1) {
+                        Console.WriteLine("Unknown error");
+                    } else if (i1 && i2 && r1 && r2) {
+                        Functions.AddGuild(command[1], guild_fund, guild_chief);
+                        Console.WriteLine("Success");
+                    }
+                }
+                else if (command[0] == "get_relation") {
+                    bool i = true, r1 = true, r2 = true;
+                    int guild_1 = -1, guild_2 = -1;
+                    try {
+                        guild_1 = Convert.ToInt32(command[1]);
+                    } catch (FormatException) {
+                        i = false;
+                    } catch (IndexOutOfRangeException) {
+                        r1 = false;
+                    }
+                    try {
+                        guild_2 = Convert.ToInt32(command[2]);
+                    } catch (FormatException) {
+                        i = false;
+                    } catch (IndexOutOfRangeException) {
+                        r2 = false;}
+                    if (!i) {
+                        Console.WriteLine("Guild id must be integer");}
+                    if (!r1) {
+                        Console.WriteLine("Guild 1 id is missing");}
+                    if (!r2) {
+                        Console.WriteLine("Guild 2 id is missing");}
+                    if (guild_1 == -1 || guild_2 == -1) {
+                        Console.WriteLine("Unknown error");
+                    } else if (i && r1 && r2) {
+                        Console.WriteLine(Functions.GetRelation(guild_1, guild_2));
+                    }
+                }
+                else if (command[0] == "alter_relation") {
+                    bool i = true, r1 = true, r2 = true;
+                    int guild_1 = -1, guild_2 = -1;
+                    try {
+                        guild_1 = Convert.ToInt32(command[1]);
+                    } catch (FormatException) {
+                        i = false;
+                    } catch (IndexOutOfRangeException) {
+                        r1 = false;
+                    }
+                    try {
+                        guild_2 = Convert.ToInt32(command[2]);
+                    } catch (FormatException) {
+                        i = false;
+                    } catch (IndexOutOfRangeException) {
+                        r2 = false;}
+                    if (!i) {
+                        Console.WriteLine("Guild id must be integer");}
+                    if (!r1) {
+                        Console.WriteLine("Guild 1 id is missing");}
+                    if (!r2) {
+                        Console.WriteLine("Guild 2 id is missing");}
+                    if (guild_1 == -1 || guild_2 == -1) {
+                        Console.WriteLine("Unknown error");
+                    } else if (i && r1 && r2) {
+                        Functions.AlterRelation(guild_1, guild_2, command[3]);
+                    }
+                }
+                else if (command[0] == "get_players") {
+                    Functions.getPlayers();
+
+                }
+                else if (command[0] == "get_guilds") {
+                    Functions.getGuilds();
+                }
+                else {
+                    Console.WriteLine("Invalid command");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
